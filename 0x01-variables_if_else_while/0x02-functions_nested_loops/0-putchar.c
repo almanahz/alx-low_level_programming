cbb8ebcdae30027f6308a6280c5d13b9e706d457
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-07-05 14:48:25 -0700

    _putchar

---
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0x02-functions_nested_loops
+ * Auth: Abdulqudus Abdulsalam
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
