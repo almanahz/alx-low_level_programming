48b40a0e228ed2138832a4841f1e10854358e33b
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-07-05 17:26:14 -0700

    sleeptime

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-  * _abs - Computes the absolute value of an integer.
-  * @c: The number to be computed.
-  *
-  * Return: Absolute value of number or zero
-  */
-
-int _abs(int c)
-
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
 {
-	if (c < 0)
-	{
-		int abs_val;
-		abs_val = c * -1;	
-		return (abs_val);
-	}
+	if (n >= 0)
+		return (n);
 	else
-		return (c);
+		return (-n);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 /**
-  * print_last_digit - Prints the last digit of a number
-  * @n: The number to be treated
-  *
-  * Return: Value of the last digit of number
-  */
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
 int print_last_digit(int n)
 {
-	int last;
+	int last_digit = n % 10;
 
-	last = n % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
 
-	if (last < 0)
-	{
-		last = last * -1;
-	}
+	_putchar(last_digit + '0');
 
-	putchar(last + '0');
-
-	return (last);
+	return (last_digit);
 }
