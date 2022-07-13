26fed323f230983294ed376bd4e1ed5a0cb82d0c
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-07-12 21:39:06 -0700

    gaols

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev -prints a string in reverse
+ * @s: a string
+ *
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
