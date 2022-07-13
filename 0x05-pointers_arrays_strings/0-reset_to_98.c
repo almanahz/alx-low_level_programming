da826735ba13c67487ee6c079b035a88f33105dd
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-07-12 21:23:27 -0700

    goal2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * _strcat - appends src to the dest string
- * @dest: string to append by src
- * @src: string to append to dest
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: parameter to take the value.
  *
- * Return: address of dest
  */
-char *_strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-	int i, j;
-
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
-	{
-		i++;
-		j++;
-	}
-	return (dest);
+	*n = 98;
 }
